ec12f8d2ae02279556fe9b633915f3ab382b30aa
itk_iantonov <iantonov@fil-it.ru>
2022-07-26 09:14:09 +0300

    task 3 | Get min price

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -1,7 +1,12 @@
 package course.concurrency.m2_async.cf.min_price;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 
 public class PriceAggregator {
 
@@ -18,7 +23,29 @@ public class PriceAggregator {
     }
 
     public double getMinPrice(long itemId) {
-        // здесь будет ваш код
-        return 0;
+        ExecutorService executorService = new ForkJoinPool();
+        double result = Double.NaN;
+        try {
+            result = CompletableFuture.supplyAsync(() -> shopIds.stream()
+                    .map(shopId -> getMinPrice(itemId, shopId, executorService))
+                    .filter(Objects::nonNull)
+                    .min(Double::compareTo)
+                    .orElse(Double.NaN))
+                .get();
+        } catch (Exception e) {
+            System.out.printf("Error while getting price for item: %d. Message: %s%n", itemId, e.getMessage());
+        } finally {
+            executorService.shutdown();
+        }
+        return result;
+    }
+
+    private Double getMinPrice(long itemId, Long shopId, ExecutorService executorService) {
+        try {
+            return executorService.submit(() -> priceRetriever.getPrice(itemId, shopId)).get(10, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            System.out.printf("Error while getting price for item: %d and shopId: %d. Exception class: %s%n", itemId, shopId, e.getClass());
+            return null;
+        }
     }
 }
