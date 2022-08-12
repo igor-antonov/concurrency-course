38313d2d76638d164e02512146037f983c4288a9
itk_iantonov <iantonov@fil-it.ru>
2022-08-12 11:05:31 +0300

    task 1.1 fix pessimistic

--- src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
@@ -15,11 +15,11 @@ public class AuctionPessimistic implements Auction {
     private volatile Bid latestBid = new Bid(0L, 0L, 0L);
 
     public boolean propose(Bid bid) {
-        if (bid.price <= latestBid.price) {
-            return false;
-        }
         lock.lock();
         try {
+            if (bid.price <= latestBid.price) {
+                return false;
+            }
             notifier.sendOutdatedMessage(latestBid);
             latestBid = bid;
         } finally {
