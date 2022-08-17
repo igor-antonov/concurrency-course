b2df92a9c658da1817762da79cc416fb5eddc25c
itk_iantonov <iantonov@fil-it.ru>
2022-08-17 11:04:06 +0300

    RestaurantService

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -1,12 +1,21 @@
 package course.concurrency.m3_shared.collections;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class RestaurantService {
 
-    private Object stat;
-    private Restaurant mockRestaurant = new Restaurant("A");
+    private final AtomicInteger counter;
+    private final ConcurrentHashMap<String, Integer> stat;
+    private final Restaurant mockRestaurant;
+
+    public RestaurantService() {
+        this.stat = new ConcurrentHashMap();
+        this.counter = new AtomicInteger(0);
+        this.mockRestaurant = new Restaurant("A");
+    }
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -14,11 +23,17 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // ваш код
+        stat.put(restaurantName, counter.incrementAndGet());
     }
 
     public Set<String> printStat() {
-        // ваш код
-        return new HashSet<>();
+        return stat.entrySet()
+            .stream()
+            .map((e) -> getStat(e.getKey(), e.getValue()))
+            .collect(Collectors.toSet());
+    }
+
+    private String getStat(String name, Integer i) {
+        return String.format("%s - %s", name, i);
     }
 }
